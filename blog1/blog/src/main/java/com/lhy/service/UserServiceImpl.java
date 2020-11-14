804c80d676a07cc4ee12a1619614e487a8e7c0cb
Luohengyi123 <391933532@qq.com>
2020-11-14 18:18:33 +0800

    blog1

---
+++ blog1/blog/src/main/java/com/lhy/service/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.lhy.service;
+
+import com.lhy.dao.UserRepository;
+import com.lhy.po.User;
+import com.lhy.util.MD5Utils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User checkUser(String username, String password) {
+        User user = userRepository.findByUsernameAndPassword(username, MD5Utils.code(password));
+        return user;
+    }
+}
